1d77ec8f329461007694ffd3c8b41a4724817cad
AmarDodda <amardodda23@gmail.com>
2024-06-20 22:35:20 +0530

    CRM Backend

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,37 +1,3 @@
-// const Feedback = require('../models/feedback');
-
-// const submitFeedback = async (request, response) => {
-//   try {
-//     const { consumerId, name, product, rating, comment, date } = request.body;
-
-//     if (!consumerId && !request.userId) {
-//       return response.status(400).json({ message: 'Consumer ID is required' });
-//     }
-
-//     const newFeedback = new Feedback({
-//       consumerId: consumerId || request.userId,
-//       name,
-//       product,
-//       rating: parseInt(rating),
-//       comment,
-//       date: date ? new Date(date) : new Date(),
-//     });
-
-//     await newFeedback.save();
-
-//     // Respond with the created feedback
-//     response.status(201).json(newFeedback);
-//   } catch (error) {
-//     console.error('Error submitting feedback:', error);
-//     response.status(500).json({ message: 'Failed to submit feedback', error: error.message });
-//   }
-// };
-
-// module.exports = {
-//   submitFeedback,
-// };
-
-
 const Feedback = require('../models/feedback');
 
 const submitFeedback = async (request, response) => {
