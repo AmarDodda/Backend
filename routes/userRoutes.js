ad5ee48772f519f9aa63895cc129f8b55b5be733
AmarDodda <amardodda23@gmail.com>
2024-06-20 22:28:10 +0530

    CRM Backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userController = require('../controllers/userController');
+const userController = require('../controllers/usercontroller');
 const router = express.Router();
 const auth = require('../middleware/auth');
 
